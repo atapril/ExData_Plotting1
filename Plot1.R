2467c1307665efe70b3edea9a58642c5f33f9ff8
Thi Allgood <44853973+atapril@users.noreply.github.com>
2019-03-24 20:31:49 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -14,15 +14,25 @@ PowerConsumption$Date <- strptime(PowerConsumption$Date, format = "%d/%m/%Y")
 PowerConsumption$Date <- as.Date(PowerConsumption$Date)
 # subset data, filter date from 2007-02-01 to 2007-02-02
 PowerConsumption <- PowerConsumption[ PowerConsumption$Date >= "2007-02-01" & PowerConsumption$Date <= "2007-02-02", ]
+#Remove NA values
 PowerConsumption <- PowerConsumption[ complete.cases(PowerConsumption),]
+# combine Date column and Time column
 Datetime <- paste(PowerConsumption$Date, PowerConsumption$Time)
+# Make DateTime a data table
 Datetime <- data.table(Datetime)
+#Name the column "DateTime"
 Datetime <- setnames(Datetime, "DateTime")
+#Remove Date column and Time column
 PowerConsumption <- PowerConsumption[ , !names(PowerConsumption) %in% c("Date","Time")]
+# Combine DateTime column into data set "PowerConsumption"
 PowerConsumption <- cbind(Datetime, PowerConsumption)
+# Make Global_active_power numeric 
 PowerConsumption$Global_active_power <- as.numeric(PowerConsumption$Global_active_power)
+# Make DateTime class date
 PowerConsumption$DateTime <- as.POSIXct(PowerConsumption$DateTime)
+# Plot 1
 hist(PowerConsumption$Global_active_power, main = "Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
 dev.copy(png,"plot1.png", width=480, height=480)
+# Close plot
 dev.off()
 
